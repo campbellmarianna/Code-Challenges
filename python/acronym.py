559880df3cb4aa2251471e36c2bc831c5ca4f997
Marianna <mckd2018@gmail.com>
2019-04-04 13:38:16 -0700

    [feat] add pseudocode for acronym - python challenge

---
+++ python/acronym.py
@@ -0,0 +1,12 @@
+# Convert a phrase to its acronym
+# Input: "Portable Network Graphics"
+# Output: PNG
+# Processing:
+    # create a variable acronym
+    # create a variable letter_location and set it to zero
+    # grab the first letter in the string, add it to acronym
+    # increment letter_location by one
+    # loop through input string starting from second character
+        # check if the character is a space
+        # if it is grab the character after that add it the acroymn
+    # return acroymn
