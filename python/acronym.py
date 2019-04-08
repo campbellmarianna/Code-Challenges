f84f1c9996c8f0d4c1c0de551086def196b43c29
Marianna <mckd2018@gmail.com>
2019-04-07 17:59:27 -0700

    [feat] complete acronym code challenge in python

--- python/acronym.py
+++ python/acronym.py
@@ -2,11 +2,24 @@
 # Input: "Portable Network Graphics"
 # Output: PNG
 # Processing:
-    # create a variable acronym
-    # create a variable letter_location and set it to zero
-    # grab the first letter in the string, add it to acronym
-    # increment letter_location by one
-    # loop through input string starting from second character
-        # check if the character is a space
-        # if it is grab the character after that add it the acroymn
-    # return acroymn
+def abbreviate(words):
+    acronym = []
+    charc_location = 0
+    first_char = words[0]
+    acronym.append(first_char)
+    charc_location += 1
+    for i in range(1, len(words)):
+        charc_location_after_space = 0
+        if words[i] == ' ' or words[i] == '-' or words[i] == '_':
+            charc_location_after_space = i + 1
+            charc_after_the_space = words[charc_location_after_space]
+            if charc_after_the_space.isalpha():
+                acronym.append(sanitize(charc_after_the_space))
+
+    return "".join(acronym)
+
+def sanitize(charc):
+    clean_charc = charc.upper()
+    return clean_charc
+
+print(abbreviate("Portable network Graphics")) # 9,17 counter = 2
