88de7922a698984f17f3b020e8319b1456173483
Marianna <mckd2018@gmail.com>
2019-06-24 17:17:41 -0700

    [fix] code syntax using linter as well for LeetCode Problem word_pattern

--- python/word_pattern.py
+++ python/word_pattern.py
@@ -1,11 +1,17 @@
-# LeetCode Problem
-'''Given a pattern and a string str, find if str follows the same pattern.
+"""
+LeetCode Problem:
+Given a pattern and a string str, find if str follows the same pattern.
 Here follow means a full match, such that there is a bijection between a letter
 in pattern and a non-empty word in str.
+Problem Source: https://leetcode.com/problems/word-pattern/
 Example:
 Input: pattern = "abba", str = "dog cat cat dog"
-Output: true'''
+Output: true
+"""
+
+
 def word_pattern(pattern, str):
+    """Return true or false by finding if the string matches the pattern."""
     counter_p = 0
     counter_str = 0
     for char_1 in pattern:
@@ -14,11 +20,13 @@ def word_pattern(pattern, str):
                 counter_p += 1
     for word_1 in str:
         for word_2 in str:
-            if word_1 == word == 2:
+            if word_1 == word_2:
                 counter_str += 1
     if counter_p == counter_str:
-        return true
-    return false
+        return True
+    return False
+
 
 if __name__ == '__main__':
-    word_pattern("abba", "pig cat pig cat")
+    print(word_pattern("abba", "pig cat pig cat"))
+    print(word_pattern("aaaa", "aaaa"))
