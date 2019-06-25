b41b13fe75f8d3fab848bee985523d2ea82ee2aa
Marianna <mckd2018@gmail.com>
2019-06-24 17:32:34 -0700

    [fix] code for class code problem using python linter flake8 as well

--- python/determine_duplicates.py
+++ python/determine_duplicates.py
@@ -1,15 +1,20 @@
-# Class Code Problem
-'''Given a list of n numbers, determine if it contains any duplicate numbers.'''
+"""
+Class Code Problem: Given a list of n numbers, determine if it contains any duplicate numbers.
+"""
 
 def check_duplicate(input_list):
-    contains_no_dups = Set()
-    for element in input_list:
-        if element in contains_no_dups:
+    """
+    Returns True or False by finding if there are any repeated elements in the inputlist.
+    """
+    contains_no_dups = set()
+    for item in input_list:
+        print("Contains_no_dups set: {}".format(contains_no_dups))
+        if item in contains_no_dups:
             return True # found deplicate
         else: # not found add to set
-            contains_no_dups.append(element)
-        return False # didn't find duplicates
+            contains_no_dups.add(item)
+    return False # didn't find duplicates
 
 if __name__ == '__main__':
-    check_duplicate([5,10,15,14])
-    check_duplicate([8,2,4,8,9,1])
+    print(check_duplicate([5,10,15,14]))
+    print(check_duplicate([8,2,4,8,9,1]))
