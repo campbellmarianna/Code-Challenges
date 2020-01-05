9e657e15627512acec70eaea923e08463493d6b0
Marianna <mckd2018@gmail.com>
2020-01-05 15:41:34 -0800

    [add] prgress on stretch python coding challenge

--- python/abcs_course_remote/mod_2_prob.py
+++ python/abcs_course_remote/mod_2_prob.py
@@ -579,12 +579,12 @@ Sample Input 2
 Sample Output 2
 5
 """
-Key action? display
-Input 
-  - First line n:int, T: int
-  - Second line one number (n) after another to symbolizing this list of how long each task will take
-Output int (number of tasks that can be completed)
-Can we manipulate passed in T? yes
+# Key action? display
+# Input 
+#   - First line n:int, T: int
+#   - Second line one number (n) after another to symbolizing this list of how long each task will take
+# Output int (number of tasks that can be completed)
+# Can we manipulate passed in T? yes
 
 # setup - put n's in list
 # loop the task time list
@@ -678,3 +678,73 @@ def server_time_check(task_config, task_times):
         else:
             break
     print(tasks_completed_counter)
+
+### Stretch Challenges
+'''
+Prompt:   Roman to Integer
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+
+
+Example 1:
+Input: "III"
+Output: 3
+
+Example 2:
+Input: "IV"
+Output: 4
+
+Example 3:
+Input: "IX"
+Output: 9
+
+Example 4:
+Input: "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+Example 5:
+Input: "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+'''
+
+# input -> Roman numeral str "IX"
+# a roman numeral with a less value can be put before a greater value to identify subtraction
+# We are just doing traslation
+# output: integer
+# roman numberals from 1 to 3999
+
+def romanToInt(s):
+    result = 0
+    # create dic keys
+    roman_num_to_int = {"I": 1, "V":5,"X": 10, "L":50, "C":100, "D": 500, "M": 1000}
+    print(roman_num_to_int)
+    # loop input
+    for sy in s:
+        value = roman_num_to_int[sy]
+        if value != -1:
+            result += value
+    # return result
+    return result
+
+
+c1 = "III"
+c2 = "IV" # smallest is written before the largest to hightlist subtraction - smallest before largest
+print(romanToInt(c1))
