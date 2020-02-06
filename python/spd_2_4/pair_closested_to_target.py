d4e34f2f370505c706d5490c965c405dbae4c151
Marianna <mckd2018@gmail.com>
2020-02-05 16:17:41 -0800

    [add] iteration #1 to solve coding challenge

---
+++ python/spd_2_4/pair_closested_to_target.py
@@ -0,0 +1,4 @@
+'''
+Given two arrays a and b of numbers and a target value t, find a number from each array whose sum is closest to t.
+Example: a=[9, 13, 1, 8, 12, 4, 0, 5],  b=[3, 17, 4, 14, 6],  t=20  =>  [13, 6] or [4, 17] or [5, 14]
+'''
