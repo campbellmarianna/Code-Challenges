9d144066bc899e1f19352e0f1d29ee89b16b8ab1
Marianna <mckd2018@gmail.com>
2020-02-06 21:20:19 -0800

    [add] k_largest_solution_idea

--- python/spd_2_4/k_largest_solutions.py
+++ python/spd_2_4/k_largest_solutions.py
@@ -14,24 +14,28 @@ def find_k_largest1(a, k):
 
 # - start a largest with the length of k
 # - interate the array and check if it is greater than any number in the largests array if so pop out the lesser value and and input the new greater value
+
 def find_k_largest2(a,k):
-    largest_k = [] # [5,1,3]
-    for i in range(k):
-        largest_k.append(a[i])
-    i = k
-    while i < len(a):
-        num = a[i]
-        print(num)
-        for largest_num in largest_k:
-            if num > largest_num:
-                largest_k.pop(largest_num)
-                largest_k.append(num)
-                print("NEW LARGEST ARR:", largest_k)
-        i += 1
-    return largest_k
+    # create a copy of the list
+    a_copy = a[:]
+    largest_vals = []
+    max_val = 0
+    # find the largest and pop it
+    while len(largest_vals) < k:
+        max_val = max(a_copy)
+        max_val_index = a_copy.index(max_val)
+        a_copy.pop(max_val_index)
+        print("max_val:", max_val)
+        largest_vals.append(max_val)
+        print("LENGTH OF ARR:", len(largest_vals))
+        # add it to the result
+        # check the length of the result that it is less than k
+            # if it is keep interating
+        # otherwise stop and return result
+    return largest_vals
 
 if __name__ == "__main__":
     a = [5, 1, 3, 6, 8, 2, 4, 7]
     k = 3
-    print(find_k_largest1(a, k)) # [6,8,7]
+    print(find_k_largest2(a, k)) # [6,8,7]
 
