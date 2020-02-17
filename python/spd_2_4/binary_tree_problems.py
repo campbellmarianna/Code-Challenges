2a0e9ab63f00b4d671db124dc0d433a59155e30a
Marianna <mckd2018@gmail.com>
2020-02-17 11:04:34 -0800

    [add] solved first Binary Tree Problem

--- python/spd_2_4/binary_tree_problems.py
+++ python/spd_2_4/binary_tree_problems.py
@@ -1,6 +1,8 @@
 '''
 Prompt:
 Given a binary search tree, reverse the order of its values by modifying the nodes’ links.
+
+Check print_reverse method for solution.
 '''
 # Edge cases:
 # - no left or no right child 
@@ -34,118 +36,68 @@ class BinarySearchTree():
     def __init__(self):
         self.root = None
     
-    def reverse1(self):
-        val_list = []
-        temp = None
-        while node is not None:
-            val_list.append(node.data)
-            if node.left:
-                node = node.left
-            elif node.right:
-                node = node.right
+    def print_bfs(self):
+        if not self.root:
+            return
+
+        queue = [self.root]
+
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            print(current_node.data)
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+
+    def print_reverse(self):
+        print("Reverse")
+        if not self.root:
+            return
+
+        queue = [self.root]
+
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            print(current_node.data)
+            if current_node.right and current_node.left:
+                tmp_right = current_node.right
+                tmp_left = current_node.left
+                current_node.left = tmp_right
+                current_node.right = tmp_left
+                queue.append(current_node.left)
+                queue.append(current_node.right)
             else:
-                break
-        return val_list
-# Binary Search Tree in Python  Credits: Joe James  https://youtu.be/YlgPi75hIBc
-class Node:
-    def __init__(self, val):
-        self.value = val
-        self.leftChild = None
-        self.rightChild = None
+                if current_node.left:
+                    queue.append(current_node.left)
+                if current_node.right:
+                    queue.append(current_node.right) 
     
-    def insert(self,data):
-        if self.value == data:
-            return False
-        elif self.value > data:
-            if self.leftChild:
-                return self.leftChild.insert(data)
-            else:
-                self.leftChild = Node(data)
-                return True 
-        else:
-            if self.rightChild:
-                return self.rightChild.insert(data)
-            else:
-                self.rightChild = Node(data)
-                return True
     
-    def find(self, data):
-        if(self.value == data):
-            return True
-        elif self.value > data:
-            if self.leftChild:
-                return self.leftChild.find(data)
-            else:
-                return False
-        else:
-            if self.rightChild:
-                return self.rightChild.find(data)
-            else:
-                return False
-    
-    def preorder(self):
-        if self:
-            print(str(self.value))
-            if self.leftChild:
-                self.leftChild.preorder()
-            if self.rightChild:
-                self.rightChild.preorder()
-    
-    def postorder(self):
-        if self:
-            if self.leftChild:
-                self.leftChild.postorder()
-            if self.rightChild:
-                self.rightChild.postorder()
-            print(str(self.value))
-    
-    def inorder(self):
-        if self:
-            if self.leftChild:
-                self.leftChild.inorder()
-            print(str(self.value))
-            if self.rightChild:
-                self.rightChild.inorder()
-
-
-
-class Tree:
-    def __init__(self):
-        self.root = None
-
-    def insert(self,data):
-        if self.root:
-            return self.root.insert(data)
-        else:
-            self.root = Node(data)
-            return True
-    
-    def find(self, data):
-        if self.root:
-            return self.root.find(data)
-        else:
-            return False
-
-    def preorder(self):
-        print("PreOrder")
-        self.root.preoder()
-
-    def postorder(self):
-        print("PostOrder")
-        self.root.postorder()
-    
-    def inorder(self):
-        print("InOrder")
-        self.root.inorder()
-
-
-            
 
 if __name__ == "__main__":
     # bst = BinarySearchTree()
-    bst = Tree()
-    bst.insert(10)
-    print(bst.insert(15))
-    bst.preorder()
-    bst.postorder()
+    tree = BinarySearchTree()
+    # Test Case 1
+    # tree.root = BinaryTreeNode(9)
+
+    # tree.root.left = BinaryTreeNode(5)
+    # tree.root.right = BinaryTreeNode(11)
+
+    # tree.root.left.left = BinaryTreeNode(3)
+    # tree.root.left.right = BinaryTreeNode(7)
     
+    # tree.print_bfs()
+    # tree.print_reverse()
+
+    # Test Case 2
+    # tree.root = BinaryTreeNode('red')
+
+    # tree.root.left = BinaryTreeNode('orange')
+    # tree.root.left.left = BinaryTreeNode('green')
+
+    # tree.root.right = BinaryTreeNode('yellow')
+    # tree.root.right.left = BinaryTreeNode('violet')
+
+    # tree.print_bfs()
+    # tree.print_reverse()
