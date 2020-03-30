eb37cfe722979a7d61679f1bca0a181a5bef0d55
Marianna <mckd2018@gmail.com>
2020-03-29 23:09:35 -0400

    [add] short curcuit evaulation summary

--- python/interview_cake/short_circuit_evaluation.py
+++ python/interview_cake/short_circuit_evaluation.py
@@ -1,12 +1,16 @@
 # Short-Circuit Evaultion
-# to avoid unneccessary work
-# if first condition is false Python 3.6 doesn't check other condition
+# To avoid unneccessary work
+
+# If first condition is false Python 3.6 doesn't check other condition
 if it_is_friday and it_is_raining:
     print("board games at my place!")
 
+# Potentially get a Key Error on the first check
 if friends['Becky'].is_free_this_friday():
     invite_to_board_game_night(friends['Becky'])
-
+# If 'Becky' isn't in friends, Python will ignore the rest of the conditional and aviod throwing the KeyError
 if 'Becky' in friends and friends['Becky'].is_free_this_friday():
     invite_to_board_game_night(friends['Becky'])
 
+# Summary
+# Short circuit evaulation enables a programmer to do related conditionals on one line instead of multiple nested conditionals. This is primary useful making code more concise and readiable.
