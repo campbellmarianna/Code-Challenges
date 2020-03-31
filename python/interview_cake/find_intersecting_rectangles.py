eeef05e7c4c74614b991da53e6cff007b918a675
Marianna <mckd2018@gmail.com>
2020-03-31 18:13:24 -0400

    [add] Intersecting rectangle helpful function

--- python/interview_cake/find_intersecting_rectangles.py
+++ python/interview_cake/find_intersecting_rectangles.py
@@ -27,9 +27,64 @@ Return the reactangle intersection within two rectangles
 # Find intersecting coordinates
 # Find bottom-left, width, and height of intersecting triangle
 
+rect_1 = {
+    # Coordinates of bottom-left corner
+    'left_x': 1,
+    'bottom_y': 1,
+
+    # Width and height
+    'width': 6, # x
+    'height': 3, # y
+}
+#  create x boundary = width + 1 = 7
+# create y boundary = height + 1 = 4
+# x = 2
+# for every width value 
+    # Get the height increment until outside of boundary 4
+    # y = 4
+[(1, 1), (1, 2), (1, 3), (1, 4),
+]
+rect_2 = {
+    # Coordinates of bottom-left corner
+    'left_x': 5,
+    'bottom_y': 2,
+
+    # Width and height
+    'width': 6,
+    'height': 3,
+}
 def find_common_coordinates(rect_1, rect_2):
     '''Return a list of coordinates found in both rectangles'''
-    return [(5, 4), (5, 3), (5, 2), (6, 2), (6, 3), (6, 4), (7, 2), (7, 3), (7,4) ]
+    # Generate all coordinates for rect 1
+    rect_1_coordinates = []
+    x_bound = rect_1['width'] + 1
+    y_bound = rect_1['height'] + 1
+    x = rect_1['left_x']
+    for _ in range(0, x_bound):
+        y = rect_1['bottom_y']
+        for _ in range(0, y_bound):
+            coordinate_pair = (x, y)
+            # add them to a list
+            rect_1_coordinates.append(coordinate_pair)
+            y += 1
+        x += 1
+    common_coordinates = []
+    # Generate all coordinates for rect 2
+    x_bound = rect_2['width'] + 1
+    y_bound = rect_2['height'] + 1
+    x = rect_2['left_x']
+    for _ in range(0, x_bound):
+        y = rect_2['bottom_y']
+        for _ in range(0, y_bound):
+            coordinate_pair = (x, y)
+            if coordinate_pair in rect_1_coordinates:
+                # add them to a list
+                common_coordinates.append(coordinate_pair)
+            y += 1
+        x += 1
+    # Return common...
+    return common_coordinates
+    # return [(5, 4), (5, 3), (5, 2), (6, 2), (6, 3), (6, 4), (7, 2), (7, 3), (7,4) ]
 
 def get_intersect_rect(common_coordinates):
     '''
@@ -39,7 +94,9 @@ def get_intersect_rect(common_coordinates):
     'width': INT,
     'height': INT
     '''
-    return my_rectangle = {'left_x':...}
+    pass
+    # return my_rectangle = {'left_x':...}
 
 
+print(find_common_coordinates(rect_1, rect_2))
 
