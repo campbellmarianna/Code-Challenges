367f08b3e18220257961cedb0a2ca0d8454d1001
Marianna <mckd2018@gmail.com>
2020-04-13 13:17:09 -0400

    [add] in progress Leetcode string compression problem

---
+++ python/interview_cake/string_compression.py
@@ -0,0 +1,85 @@
+'''
+Given an array of characters, compress it in-place.
+
+The length after compression must always be smaller than or equal to the original array.
+
+Every element of the array should be a character (not int) of length 1.
+
+After you are done modifying the input array in-place, return the new length of the array.
+
+ 
+Follow up:
+Could you solve it using only O(1) extra space?
+
+ 
+Example 1:
+
+Input:
+["a","a","b","b","c","c","c"]
+
+Output:
+Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+
+Explanation:
+"aa" is replaced by "a2". "bb" is replaced by "b2". "ccc" is replaced by "c3".
+ 
+
+Example 2:
+
+Input:
+["a"]
+
+Output:
+Return 1, and the first 1 characters of the input array should be: ["a"]
+
+Explanation:
+Nothing is replaced.
+ 
+
+Example 3:
+
+Input:
+["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+
+Output:
+Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
+
+Explanation:
+Since the character "a" does not repeat, it is not compressed. "bbbbbbbbbbbb" is replaced by "b12".
+Notice each digit has it's own entry in the array.
+ 
+
+Note:
+
+All characters have an ASCII value in [35, 126].
+1 <= len(chars) <= 1000.
+'''
+# Given list a characters return a compressed version of the list
+
+# Details
+# Implement soluiton in place
+# the character and the number of occurances are side by side
+# if it only appears once don't compress
+# Each digit has it's own entry in the array # => ["a","b","1","2"]
+
+# Soluton Ideas
+# dictionary also used a list of tuples that will stay in order
+# add characters and occurances to dictionary
+# loop through list and dict
+# find occurances value
+# delete the occurances
+# Big O Space: 0(u) for u characters in the given list
+# Big O Time: 0(u) for u characters in the given list
+
+# loop list
+# look starting from next index
+# check if curr character is seen
+
+# Big O Space: 0(1)
+# Big O Time: 0(n^2) for u characters in the given list
+
+# ["a"] = > 1
+def compress(self, chars: List[str]) -> int:
+    # loop list
+    # look starting from next index
+    # check if curr character is seen
