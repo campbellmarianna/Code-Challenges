79541a93ee7742b73479016b4a09769a99f70073
Marianna <mckd2018@gmail.com>
2020-05-07 22:03:46 -0400

    [add] initial attempt to parsing Json data

--- python/backend-fullstack/parse_json.py
+++ python/backend-fullstack/parse_json.py
@@ -8,3 +8,24 @@ topology/pod-1/node-201/sys/phys-[eth1/33]                              inherit 
 topology/pod-1/node-201/sys/phys-[eth1/34]                              inherit   9150 
 topology/pod-1/node-201/sys/phys-[eth1/35]                              inherit   9150 
 '''
+import json
+
+# Display static content
+print(
+'''
+Interface Status
+================================================================================
+DN                                                 Description           Speed    MTU  
+-------------------------------------------------- --------------------  ------  ------
+'''
+)
+# open up the file 
+with open('exer1-interface-data.json', 'r') as f:
+    data_dict = json.load(f)
+    for data in data_dict:
+        print(f'{data["dn"]}         {data["descr"]} {data["speed"]}   {data["mut"]}')
+
+# loop and print relevant data
+
+
+
